861c550c1ea63480e4d1dc3f2c8fed4b012c1b35
Ilona Shcherbakova <shcherbakova0911@mail.ru>
2019-09-19 15:41:24 +0300

    Initial Commit

---
+++ HelloWorld2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld2
+//
+//  Created by Ilona Shcherbakova on 19/09/2019.
+//  Copyright © 2019 Ilona Shcherbakova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
